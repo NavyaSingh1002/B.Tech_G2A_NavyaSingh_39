0427134020edd2cf489b9d04ffc861d216aeb25b
NavyaSingh1002 <navya102007@gmail.com>
2026-04-02 01:10:56 +0530

    Add files via upload

---
+++ Buzz_number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n%7==0 || n%10==7)
+    {
+        printf("%d is a buzz number.", n);
+    }
+    else
+    {
+        printf("%d is not a buzz number.", n);
+    }   
+    return 0;
+}
