bdb0a75dd45e99861ebe4b972c9d8dfa5ce44932
Navya Singh <navya102007@gmail.com>
2026-04-21 22:51:09 +0530

    Add files via upload

---
+++ AGE_eligibilty.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter age: ");
+    scanf("%d", &age);
+    if(age >= 18)
+    {
+        printf("You are an adult.");
+    }
+    else
+    {
+        printf("You are not an adult.");
+    }
+    return 0;
+}
